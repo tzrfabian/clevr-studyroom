45f245ea30979e812e0ca025c4df68e83bd3dcef
Fabian Tenzara <fabiantenzara@gmail.com>
2024-10-08 11:49:28 +0700

    refactor: change logo on navbar & add logo in Homepage

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
       <h1 className="text-5xl font-bold mb-6 transform transition-transform duration-700 hover:scale-110 animate-bounce">
         Welcome to Clevr
       </h1>
+      <img src="https://cdn.discordapp.com/attachments/1289154509993414729/1290973323056123984/clevr_logo_1_1.png?ex=6705a859&is=670456d9&hm=00fd4daa31793ed56e05327075785da20012d53c251ac085f6f87752fd558dae&" alt="img" width={350}/>
       <p className="text-2xl mb-4 transition-opacity duration-500 hover:opacity-80 animate-fade-in-delayed">
         {user
           ? `Hello, ${user.email}!`
